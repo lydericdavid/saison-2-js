e8425be42d0a74b5e5418c2d286109c903d4cb7b
Nve Hounkponou Orphée <norpheehounnkponou@gmail.com>
2020-05-16 13:27:22 +0100

    #test jasmine

--- app.js
+++ app.js
@@ -3,10 +3,26 @@ var email = "orpheenve@hotmail.com";
 verifier_email ("toto;tutu;titi;")
 verifier_email(email);
 
-
-function verifier_email(email) {
-    var parts = email.split('@');
-    console.log (parts);
-
+/**
+ * Verifie si l'adresse email passé en paramettre est correcte
+ * @param {String} email
+ * @returns {Boolean} 
+ */
+function verifierEmail(email) {
+    //1. soit email supposé être email = xxxx@yyy.zzz
+    //2. Verifier si email est de la forme xxxx@yyy
+    //3. Verifier si mail est de la form yyyy.zzz
+    //4. Verifer si email ne contient pas d'espace
 }
 
+function hashArobaseOnMiddle(str){
+    return false;
+}
+
+function hashPointOnMiddle(str){
+    return false;
+}
+
+function hasNotSpace(str){
+    return false;
+}
